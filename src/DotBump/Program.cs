144fdb3f35642d4a686807878fc457ce94b45d86
Roby Van Damme <123795+robyvandamme@users.noreply.github.com>
2025-07-03 09:01:34 +0700

    config: configure solution and projects (#3)
    Configure the solution and projects:
    
    * Add a console and test project
    * Configure code style and copyright text
    * Configure GitVersion with Conventional Commits
    * Add default NuGet packages
    
    Closes #1

---
+++ src/DotBump/Program.cs
@@ -0,0 +1,3 @@
+﻿// Copyright © 2025 Roby Van Damme.
+
+Console.WriteLine("Hello, World!");
---
+++ test/DotBump.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+// Copyright © 2025 Roby Van Damme.
+
+namespace DotBump.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
