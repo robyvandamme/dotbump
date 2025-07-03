8545b750f660ba6947ad0627d9e62b575aefa709
Roby Van Damme <roby@robyvandamme.com>
2025-07-03 09:42:41 +0700

    Configure CI build

--- build/Build.cs
+++ build/Build.cs
@@ -1,44 +1,117 @@
-using System;
+// Copyright © 2025 Roby Van Damme.
+
+using System.IO;
 using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
+using Nuke.Common.Tools.DotNet;
+using Serilog;
+
+namespace DotBump.Build;
 
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
-
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    Target Clean => _ => _
-        .Before(Restore)
+    [Solution]
+    readonly Solution Solution;
+
+    readonly AbsolutePath ArtifactsDirectory = RootDirectory / "artifacts";
+
+    readonly AbsolutePath TestReportsDirectory = RootDirectory / "artifacts" / "test-reports";
+
+    [PathVariable]
+    readonly Tool dotnet;
+
+    [Parameter]
+    readonly string NuGetApiKey;
+
+    [Parameter]
+    readonly string NuGetFeed;
+
+    [Parameter]
+    readonly bool PackAndPublish;
+
+    Target RestoreTools => t => t
         .Executes(() =>
         {
+            Log.Information("Restoring tools");
+            DotNetTasks.DotNetToolRestore();
         });
 
-    Target Restore => _ => _
+    Target Clean => t => t
+        .Requires(() => Solution)
+        .DependsOn(RestoreTools)
         .Executes(() =>
         {
+            Log.Information("Cleaning solution: {Solution}", Solution.Path);
+            DotNetTasks.DotNetClean(o => o.SetProject(Solution.Path));
         });
 
-    Target Compile => _ => _
+    Target Restore => t => t
+        .Requires(() => Solution)
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+            Log.Information("Restoring solution: {Solution}", Solution.Path);
+            DotNetTasks.DotNetRestore(o => o.SetProjectFile(Solution.Path));
+        });
+
+    Target Compile => t => t
+        .Requires(() => Solution)
         .DependsOn(Restore)
+        .Triggers(RunTests)
         .Executes(() =>
         {
+            Log.Information("Building solution: {Solution}", Solution.Path);
+            DotNetTasks.DotNetBuild(o => o
+                .SetProjectFile(Solution.Path)
+                .SetConfiguration(Configuration));
         });
 
+    Target RunTests => t => t
+        .Requires(() => Solution)
+        .Executes(() =>
+        {
+            Log.Information("Looking for tests in solution");
+            var tests = Solution.AllProjects.Where(p => p.Name.Contains("Tests")).ToList();
+            if (tests.Any())
+            {
+                var reportsDirectory = new DirectoryInfo(TestReportsDirectory);
+                if (!reportsDirectory.Exists)
+                {
+                    reportsDirectory.Create();
+                }
+                else
+                {
+                    reportsDirectory.Delete(true);
+                    reportsDirectory.Create();
+                }
+
+                foreach (var test in tests)
+                {
+                    Log.Information("Executing tests: {Project}", test.Path);
+                    DotNetTasks.DotNetTest(o => o
+                        .SetProjectFile(test.Path)
+                        .EnableNoBuild()
+                        .SetConfiguration(Configuration)
+                        .SetDataCollector("XPlat Code Coverage")
+                        .SetResultsDirectory(TestReportsDirectory)
+                        .SetLoggers($"html;logfilename={TestReportsDirectory}/testResults.html"));
+                }
+
+                Log.Information("Creating coverage reports");
+                dotnet(
+                    $"reportgenerator -reports:{TestReportsDirectory}/*/*.xml -targetdir:{TestReportsDirectory}/coverage");
+            }
+            else
+            {
+                Log.Information("No tests found in solution");
+            }
+        });
 }
