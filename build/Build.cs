53dccdb513f370ea14a6e4c3bc3acab3d1bc23e4
Roby Van Damme <roby@robyvandamme.com>
2025-07-25 09:07:17 +0700

    Copy over build stuff from dotbot

--- build/Build.cs
+++ build/Build.cs
@@ -28,11 +28,35 @@ class Build : NukeBuild
     [PathVariable]
     readonly Tool dotnet;
 
+    [Parameter]
+    readonly string NuGetApiKey;
+
+    [Parameter]
+    readonly string NuGetFeed;
+
+    [Parameter]
+    readonly bool PackAndPublish;
+
     Target RestoreTools => t => t
         .Executes(() =>
         {
             Log.Information("Restoring tools");
             DotNetTasks.DotNetToolRestore();
+        });
+
+    /// <summary>
+    /// Sets the version properties for all the projects using the current GitVersion projected version.
+    /// </summary>
+    Target SetVersion => t => t
+        .DependsOn(RestoreTools)
+        .OnlyWhenStatic(() => PackAndPublish)
+        .Executes(() =>
+        {
+            // var version = GitVersionTasks.GitVersion().Result.SemVer;
+            // Log.Information("Setting version: {Version}", version);
+
+            // NOTE: for some reason /updateprojectfiles only works (locally) when I add the verbosity argument...
+            // GitVersionTasks.GitVersion($"path {Solution.Directory} /verbosity Normal /updateprojectfiles");
         });
 
     Target Clean => t => t
@@ -56,6 +80,7 @@ class Build : NukeBuild
     Target Compile => t => t
         .Requires(() => Solution)
         .DependsOn(Restore)
+        .DependsOn(SetVersion)
         .Triggers(RunTests)
         .Executes(() =>
         {
@@ -103,6 +128,38 @@ class Build : NukeBuild
             else
             {
                 Log.Information("No tests found in solution");
+            }
+        });
+
+    Target Pack => t => t
+        .Triggers(Publish)
+        .OnlyWhenStatic(() => PackAndPublish)
+        .Executes(() =>
+        {
+            Log.Information("Packing...");
+            DotNetTasks.DotNetPack(o => o
+                .SetNoBuild(true)
+                .SetConfiguration(Configuration)
+                .SetProject($"{Solution.Directory}/src/DotBot/DotBot.csproj")
+                .SetOutputDirectory(ArtifactsDirectory));
+        });
+
+    Target Publish => t => t
+        .Requires(() => NuGetFeed, () => NuGetApiKey)
+        .Executes(() =>
+        {
+            Log.Information("Publishing...");
+            var packagePath = $"{ArtifactsDirectory}/*.nupkg";
+
+            if (!IsLocalBuild)
+            {
+                // For pushing we use a PAT for now since passing in the GITHUB_TOKEN did not work in initial testing.
+                dotnet($"nuget push -s {NuGetFeed} -k {NuGetApiKey} {packagePath}");
+            }
+            else
+            {
+                // push to the local package feed
+                dotnet($"nuget push -s http://localhost:9500/v3/index.json {packagePath} --skip-duplicate");
             }
         });
 }
