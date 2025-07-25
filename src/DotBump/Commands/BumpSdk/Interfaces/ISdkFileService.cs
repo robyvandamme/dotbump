f2d31256ddebb83ae8c33d48658ecc19006d2294
Roby Van Damme <roby@robyvandamme.com>
2025-07-25 08:43:17 +0700

    Cleanup after review

--- src/DotBump/Commands/BumpSdk/Interfaces/ISdkFileService.cs
+++ src/DotBump/Commands/BumpSdk/Interfaces/ISdkFileService.cs
@@ -1,10 +1,12 @@
 // Copyright © 2025 Roby Van Damme.
 
+using DotBump.Commands.BumpSdk.DataModel;
+
 namespace DotBump.Commands.BumpSdk.Interfaces;
 
 internal interface ISdkFileService
 {
     void UpdateSdkVersion(string oldSdkVersion, string newSdkVersion, string filePath);
 
-    DataModel.Sdk GetCurrentSdkVersionFromFile(string filePath);
+    Sdk GetCurrentSdkVersionFromFile(string filePath);
 }
