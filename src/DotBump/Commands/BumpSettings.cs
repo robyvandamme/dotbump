0a63a6abded8638d51124158e47d4fd89c613e45
Roby Van Damme <roby@robyvandamme.com>
2025-07-29 09:07:50 +0700

    Update settings documentation

--- src/DotBump/Commands/BumpSettings.cs
+++ src/DotBump/Commands/BumpSettings.cs
@@ -12,8 +12,8 @@ internal abstract class BumpSettings : CommandSettings
 {
     /// <summary>
     /// Gets or sets the debug flag.
-    /// Note that this particular Spectre command setting is not required since the argument gets picked up by the
-    /// <see cref="ArgumentHandler"/> when the application is starting up but is useful for documentation purposes.
+    /// Note that this particular Spectre command setting is required primarily for documentation purposes and example
+    /// validation. The argument is handled by the <see cref="ArgumentHandler"/> when the application is starting up .
     /// The reason this is handled outside of Spectre is to be able to hande the `--debug` argument anywhere in the
     /// argument list which seemed to not be possible with the default Spectre approach and to pick it up as early as
     /// possible to configure logging.
@@ -25,8 +25,8 @@ internal abstract class BumpSettings : CommandSettings
 
     /// <summary>
     /// Gets or sets the file to send the log output to.
-    /// Note that this particular Spectre command setting is not required since the argument gets picked up by the
-    /// <see cref="ArgumentHandler"/> when the application is starting up.
+    /// Note that this particular Spectre command setting is required primarily for documentation purposes and example
+    /// validation. The argument is handled by the <see cref="ArgumentHandler"/> when the application is starting up .
     /// The reason this is handled outside of Spectre is to be able to hande the `--logfile` argument anywhere in the
     /// argument list which seemed to not be possible with the default Spectre approach and to pick it up as early as
     /// possible to configure logging.
