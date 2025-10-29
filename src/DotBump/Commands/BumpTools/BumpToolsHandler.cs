6c3ce8c21bd66f811e606ace52e4c317ecd944df
Roby Van Damme <roby@robyvandamme.com>
2025-10-29 17:38:24 +0700

    feat: secure feeds - wip
    Allow connecting to secure feeds. Requires getting the credentials from the environment.

--- src/DotBump/Commands/BumpTools/BumpToolsHandler.cs
+++ src/DotBump/Commands/BumpTools/BumpToolsHandler.cs
@@ -1,5 +1,6 @@
 // Copyright © 2025 Roby Van Damme.
 
+using System.Net;
 using DotBump.Commands.BumpSdk;
 using DotBump.Commands.BumpTools.Interfaces;
 using DotBump.Common;
@@ -40,69 +41,118 @@ internal class BumpToolsHandler(
         // so this needs to include the credentials as well
         foreach (var nugetPackageSource in nuGetConfiguration.PackageSources)
         {
-        }
+            // NOTE: this should be a using
 
-        var indexes = await nuGetServiceClient.GetServiceIndexesAsync(nuGetConfiguration)
-            .ConfigureAwait(false);
-        var baseUrls = nuGetReleaseService.GetRegistrationsUrls(indexes);
-
-        for (var i = 0; i < manifest.Tools.Count; i++)
-        {
-            var tool = manifest.Tools.ElementAt(i);
-            var releaseIndex =
-                await nuGetServiceClient.GetPackageInformationAsync(baseUrls, tool.Key).ConfigureAwait(false);
-
-            if (releaseIndex != null)
+            nuGetConfiguration.Credentials.TryGetValue(nugetPackageSource.Key, out var sourceCredential);
+            if (sourceCredential != null)
             {
-                var pages = nuGetReleaseService.TryFindNewReleaseCatalogPages(
-                    releaseIndex,
-                    tool.Value.SemanticVersion);
+                var userNamePlaceHolder = sourceCredential.Credentials.FirstOrDefault(o => o.Key.Equals(
+                    "UserName",
+                    StringComparison.OrdinalIgnoreCase));
+                var passwordPlaceHolder = sourceCredential.Credentials.FirstOrDefault(o => o.Key.Equals(
+                    "ClearTextPassword",
+                    StringComparison.OrdinalIgnoreCase));
+                if (userNamePlaceHolder != null && passwordPlaceHolder != null)
+                {
+                    var userNameVariable = userNamePlaceHolder.Value.Replace("%", string.Empty);
+                    var passwordVariable = passwordPlaceHolder.Value.Replace("%", string.Empty);
 
-                if (pages.Count == 0)
-                {
-                    logger.Debug("No new versions found in catalog for {Tool}", tool);
-                    break;
-                }
+                    var userName = Environment.GetEnvironmentVariable(userNameVariable);
+                    var password = Environment.GetEnvironmentVariable(passwordVariable);
+                    if (string.IsNullOrWhiteSpace(userName))
+                    {
+                        logger.Debug(
+                            "Environment variable {UserName} not found for {PackageSource}",
+                            userNameVariable,
+                            nugetPackageSource.Key);
+                    }
 
-                // then there are 2 options?
-                // either the release info is in the release index itself
-                // or the release info is in a page linked from the release index
-                if (pages.First().HasPackageDetails)
-                {
-                    // we can extract the version from the pages
-                    var newVersion =
-                        nuGetReleaseService.TryGetNewMinorOrPatchVersionFromCatalogPages(
-                            pages,
-                            tool.Value.SemanticVersion);
-                    if (newVersion != null)
+                    if (string.IsNullOrWhiteSpace(password))
                     {
-                        bumpToolResults.Add(new BumpToolResult(tool.Key, tool.Value.Version, newVersion.ToString()));
-                        tool.Value.Version = newVersion.ToString();
+                        logger.Debug(
+                            "Environment variable {Password} not found for {PackageSource}",
+                            passwordVariable,
+                            nugetPackageSource.Key);
                     }
-                }
-                else
-                {
-                    // TODO: get the relevant detail pages
-                    //  However... it might be possible to pick up the version from the index?
-                    //  in case the upper one is one that we can use? If the upper one is one we can use we pick that one
-                    //  otherwise we need to dig deeper.
-                    //  So: first check if we have a usable version available here: that would be the last available version only?
-                    //  if not fetch the relevant pages.
-                    //  For now: fetch the relevant pages. Add the optimization to the backlog.
-                    //  That would be Pages.Last.HasMatchingVersion or something along those lines
-                    var detailPages = await nuGetServiceClient.GetRelevantDetailCatalogPagesAsync(pages);
-                    var newVersion =
-                        nuGetReleaseService.TryGetNewMinorOrPatchVersionFromDetailCatalogPages(
-                            detailPages.ToList(),
-                            tool.Value.SemanticVersion);
-                    if (newVersion != null)
-                    {
-                        bumpToolResults.Add(new BumpToolResult(tool.Key, tool.Value.Version, newVersion.ToString()));
-                        tool.Value.Version = newVersion.ToString();
-                    }
+
+                    var handler = new HttpClientHandler { Credentials = new NetworkCredential(userName, password), };
+                    using var client = new HttpClient(handler);
+                    var response = await client.GetStringAsync(new Uri(nugetPackageSource.Value));
                 }
             }
+
+            //     // this needs to become a credential and needs to be excluded from logs
+            //     var usernamePlaceHolder = nuGetConfiguration.Credentials.Values.
+            //     var userName = Environment.GetEnvironmentVariable("GITHUB_PACKAGES_USER");
+            // }
+            //
+            // var handler = new HttpClientHandler
+            // {
+            //     Credentials = new NetworkCredential(userName, password),
+            // };
         }
+
+        // var indexes = await nuGetServiceClient.GetServiceIndexesAsync(nuGetConfiguration)
+        //     .ConfigureAwait(false);
+        // var baseUrls = nuGetReleaseService.GetRegistrationsUrls(indexes);
+        //
+        // for (var i = 0; i < manifest.Tools.Count; i++)
+        // {
+        //     var tool = manifest.Tools.ElementAt(i);
+        //     var releaseIndex =
+        //         await nuGetServiceClient.GetPackageInformationAsync(baseUrls, tool.Key).ConfigureAwait(false);
+        //
+        //     if (releaseIndex != null)
+        //     {
+        //         var pages = nuGetReleaseService.TryFindNewReleaseCatalogPages(
+        //             releaseIndex,
+        //             tool.Value.SemanticVersion);
+        //
+        //         if (pages.Count == 0)
+        //         {
+        //             logger.Debug("No new versions found in catalog for {Tool}", tool);
+        //             break;
+        //         }
+        //
+        //         // then there are 2 options?
+        //         // either the release info is in the release index itself
+        //         // or the release info is in a page linked from the release index
+        //         if (pages.First().HasPackageDetails)
+        //         {
+        //             // we can extract the version from the pages
+        //             var newVersion =
+        //                 nuGetReleaseService.TryGetNewMinorOrPatchVersionFromCatalogPages(
+        //                     pages,
+        //                     tool.Value.SemanticVersion);
+        //             if (newVersion != null)
+        //             {
+        //                 bumpToolResults.Add(new BumpToolResult(tool.Key, tool.Value.Version, newVersion.ToString()));
+        //                 tool.Value.Version = newVersion.ToString();
+        //             }
+        //         }
+        //         else
+        //         {
+        //             // TODO: get the relevant detail pages
+        //             //  However... it might be possible to pick up the version from the index?
+        //             //  in case the upper one is one that we can use? If the upper one is one we can use we pick that one
+        //             //  otherwise we need to dig deeper.
+        //             //  So: first check if we have a usable version available here: that would be the last available version only?
+        //             //  if not fetch the relevant pages.
+        //             //  For now: fetch the relevant pages. Add the optimization to the backlog.
+        //             //  That would be Pages.Last.HasMatchingVersion or something along those lines
+        //             var detailPages = await nuGetServiceClient.GetRelevantDetailCatalogPagesAsync(pages);
+        //             var newVersion =
+        //                 nuGetReleaseService.TryGetNewMinorOrPatchVersionFromDetailCatalogPages(
+        //                     detailPages.ToList(),
+        //                     tool.Value.SemanticVersion);
+        //             if (newVersion != null)
+        //             {
+        //                 bumpToolResults.Add(new BumpToolResult(tool.Key, tool.Value.Version, newVersion.ToString()));
+        //                 tool.Value.Version = newVersion.ToString();
+        //             }
+        //         }
+        //     }
+        // }
 
         // TODO: only save when there are changes.
         // do we always have results? depends on the strategy.
