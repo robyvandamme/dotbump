0e68fb12e9be28c29aa694a0a4c1bde7f38a5adb
Roby Van Damme <roby@robyvandamme.com>
2026-09-22 09:09:52 +0200

    chore: copy over common functionality
    Copy over the latest common functionality from the dotadr project.

---
+++ src/DotBump/Common/LoggerConfigurator.cs
@@ -0,0 +1,47 @@
+﻿// Copyright © Roby Van Damme.
+
+using System.Globalization;
+using DotBump;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace DotBump.Common;
+
+internal static class LoggerConfigurator
+{
+    internal static void Configure(string[] args)
+    {
+        var defaultLevelSwitch = new LoggingLevelSwitch(LogEventLevel.Error);
+
+        if (ArgumentHandler.IsDebugMode(args))
+        {
+            defaultLevelSwitch.MinimumLevel = LogEventLevel.Debug;
+        }
+
+        var logFile = ArgumentHandler.LogFile(args);
+
+        if (!string.IsNullOrWhiteSpace(logFile))
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.ControlledBy(defaultLevelSwitch)
+#if DEBUG
+                .WriteTo.Console(formatProvider: CultureInfo.InvariantCulture)
+#endif
+                .WriteTo.File(
+                    logFile,
+                    rollingInterval: RollingInterval.Day,
+                    formatProvider: CultureInfo.InvariantCulture)
+                .CreateLogger();
+        }
+        else
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.ControlledBy(defaultLevelSwitch)
+#if DEBUG
+                .WriteTo.Console(formatProvider: CultureInfo.InvariantCulture)
+#endif
+                .CreateLogger();
+        }
+    }
+}
