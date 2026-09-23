4631920d934601c6cbe5e8ce42923fa51402d071
Roby Van Damme <roby@robyvandamme.com>
2026-09-23 09:16:10 +0200

    fix: update command configuration to include version info (#63)
    Set the ApplicationVersion required for using the built-in `--version`
    command, fix the CommandConfiguration null checks and add a general
    Usage section to the readme that includes the default tool options.

--- src/DotBump/CommandConfiguration.cs
+++ src/DotBump/CommandConfiguration.cs
@@ -1,10 +1,10 @@
 // Copyright © Roby Van Damme.
 
-using System.Diagnostics;
 using DotBump.Commands.BumpSdk;
 using DotBump.Commands.BumpSdk.Interfaces;
 using DotBump.Commands.BumpTools;
 using DotBump.Commands.BumpTools.Interfaces;
+using DotBump.Common;
 using Serilog;
 using Spectre.Console.Cli;
 
@@ -12,12 +12,12 @@ namespace DotBump;
 
 internal static class CommandConfiguration
 {
-    internal static void Configure(this CommandApp commandApp, ILogger logger)
+    internal static void Configure(this CommandApp commandApp, ILogger logger, VersionInfo versionInfo)
     {
-        ArgumentNullException.ThrowIfNull(nameof(commandApp));
-        ArgumentNullException.ThrowIfNull(nameof(logger));
+        ArgumentNullException.ThrowIfNull(commandApp);
+        ArgumentNullException.ThrowIfNull(logger);
+        ArgumentNullException.ThrowIfNull(versionInfo);
 
-        Debug.Assert(commandApp != null, nameof(commandApp) + " != null");
         commandApp.Configure(config =>
         {
 #if DEBUG
@@ -25,6 +25,7 @@ internal static class CommandConfiguration
             config.ValidateExamples();
 #endif
             config.SetApplicationName("dotbump");
+            config.SetApplicationVersion(versionInfo.Version ?? "not available");
             config.Settings.Registrar.RegisterInstance(logger);
 
             config.Settings.Registrar.Register<ISdkFileService, SdkFileService>();
